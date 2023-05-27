29076c6631b81bfabcb50a0e7ccbc922e1e9972d
n06578 <n06578@gmail.com>
2023-05-27 18:26:36 +0900

    db connetion change

--- configure/db_con.php
+++ configure/db_con.php
@@ -1,9 +1,9 @@
 <?php 
 include $_SERVER['DOCUMENT_ROOT'] . "/configure/mysql.php";
-$host='everytest.dothome.co.kr';												# 호스트명 또는 IP
-$user='everytest';												# Mysql 유저이름
-$dbpass='Nyoun003310!';												# DB 패스워드
-$dbname='everytest';	
+$host='localhost';												# 호스트명 또는 IP
+$user='n06578';												# Mysql 유저이름
+$dbpass='skdbsdkWkd';												# DB 패스워드
+$dbname='n06578';	
 										# 데이타 베이스 이름
 
 $sock = mysql_connect($host,$user,$dbpass);
