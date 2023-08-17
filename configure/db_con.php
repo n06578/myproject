e4dd932cddc74964d18f9b8ceee06792250d32d6
n06578 <n06578@gmail.com>
2023-08-17 15:02:19 +0900

    server error

--- configure/db_con.php
+++ configure/db_con.php
@@ -1,14 +1,9 @@
 <?php 
 include $_SERVER['DOCUMENT_ROOT'] . "/configure/mysql.php";
-$host='localhost';												# 호스트명 또는 IP
-$user='n06578';												# Mysql 유저이름
-$dbpass='skdbsdkWkd';												# DB 패스워드
-$dbname='n06578';	
+// $host='localhost';												# 호스트명 또는 IP
+// $user='n06578';												# Mysql 유저이름
+// $dbpass='skdbsdkWkd';												# DB 패스워드
+// $dbname='n06578';	
 										# 데이타 베이스 이름
-
-$sock = mysql_connect($host,$user,$dbpass);
-$db = mysql_select_db($dbname,$sock);
-
-mysql_query('set names utf8');
-
-?>
+										
+										
