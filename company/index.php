27912ecf04aea4d934d32216509701703bf8d5dd
na younah <n06578@gmail.com>
2023-08-17 17:44:56 +0900

    회사 관련 자주 사용 함수

--- company/index.php
+++ company/index.php
@@ -1,9 +1,68 @@
+<?
+$root ="../";
+?>
+
 <!DOCTYPE html>
 <html> 
     <head>
-        <script type="text/javascript" src="youn.js"></script>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <script type="text/javascript" src="<?=$root?>js/jquery-2.0.0.min.js"></script>
+        <script type="text/javascript" src="<?=$root?>css/all/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="<?=$root?>setting/js/polyfill.js"></script>
+        <script type="text/javascript" src="<?=$root?>setting/js/runtime.js"></script>
+        <script type="text/javascript" src="<?=$root?>setting/js/script.js"></script>
+        
     </head>
     <body>
+        <div>
+            <input type="button" value="smallPopup" onclick="smallPopup()"/>
+            <br>
+            <input type="button" value="mediumPopup" onclick="mediumPopup()"/>
+            <br>
+            <input type="button" value="largePopup" onclick="largePopup()"/>
+        </div>
+        <br>
+        <div style="width:100%;display:flex">
+            <div style="width:40%;background-color: #1f1f1f0d;">
+                <table border=1 width="100%" id="table">
+                    <colspan>
+                        <col width="10%">
+                        <col width="30%">
+                        <col width="30%">
+                        <col width="30%">
+                    </colspan>
+                    <tr>
+                        <th><input type="checkbox" id="checkall"></th>
+                        <th>1</th>
+                        <th>2</th>
+                        <th>3</th>
+                    </tr>
+                    <?for($i=0;$i<10;$i++){?>
+                    <tr>
+                        <td align=center>
+                            <input type="checkbox" name="chk[]" value="<?=$i?>">
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <input type="button" class="btn-del" value="행 삭제" onclick="delRow(this)">
+                        </td>
+                    <?
+                    }
+                    ?>
+                    </tr>
+                </table>
+            </div>
+            <div style="width:40%;background-color: #1f1f1f30;">
+                <input type="button" value="결과보기" onclick="resultPrint();">
+                <input type="button" value="테이블 tr 추가" onclick="addRow();">
+                <br>
+                <div id="resultBox">
 
+                </div>
+            </div>
+        </div>
     </body>
 </html>
+
+<script type="text/javascript" src="youn.js" charset="utf-8"></script>
--- company/youn.js
+++ company/youn.js
@@ -6,15 +6,46 @@ $("#checkall").click(function () {
     }
 });
 
+function resultPrint(){
+	$("#resultBox").html(checkVal());
+}
 
 function checkVal(){
-    var chk_count = 0;
 	var chkSeq="";
 	$("input[name='chk[]']:checked").each(function() {
         var val=$(this).val();
         chkSeq +=val+",";
     });
-	return chkSeq.sclice(0,-1);
+	if(chkSeq!=""){
+		return chkSeq.slice(0,-1);
+	}
+}
+
+
+function addRow() {
+	// table element 찾기
+	const table = document.getElementById('table');
+	var checkbox_index = $("#table tr").length;
+	console.log(checkbox_index-1);
+	$("#resultBox").append(checkbox_index+"<br>");
+	
+	// 새 행(Row) 추가
+	const newRow = table.insertRow();
+	
+	// 새 행(Row)에 Cell 추가
+	const newCell1 = newRow.insertCell(0);
+	const newCell2 = newRow.insertCell(1);
+	const newCell3 = newRow.insertCell(2);
+	const newCell4 = newRow.insertCell(3);
+	
+	
+	newCell1.innerHTML = "<input type=\"checkbox\" name=\"chk[]\"  value="+checkbox_index+">";
+	newCell4.innerHTML = "<input type=\"button\" class=\"btn-del\" value=\"행 삭제\" onclick=\"delRow(this)\">";
+	
+  }
+
+function delRow(row){
+	row.closest("tr").remove();
 }
 
 
@@ -25,8 +56,35 @@ function smallPopup(){
 	var posy = 0
 	posx = (screen.width - width)/2-1;
 	posy = (screen.height - height)/2-1;
-	url = "cals_popup.php?" + query_string;
-	newwin = window.open(url,"setting","width="+width+",height="+height+",toolbar=0,scrollbars=1,resizable=0,status=0");
+	url = "test.php?mode=smallPopup";
+	newwin = window.open(url,"","width="+width+",height="+height+",toolbar=0,scrollbars=1,resizable=0,status=0");
+	newwin.moveTo(posx,posy);
+	newwin.focus();
+}
+
+
+function mediumPopup(){
+	var width=800;
+	var height=450;
+	var posx = 0
+	var posy = 0
+	posx = (screen.width - width)/2-1;
+	posy = (screen.height - height)/2-1;
+	url = "test.php?mode=midiumpopup";
+	newwin = window.open(url,"","width="+width+",height="+height+",toolbar=0,scrollbars=1,resizable=0,status=0");
+	newwin.moveTo(posx,posy);
+	newwin.focus();
+}
+
+function largePopup(){
+	var width=1000;
+	var height=750;
+	var posx = 0
+	var posy = 0
+	posx = (screen.width - width)/2-1;
+	posy = (screen.height - height)/2-1;
+	url = "test.php?mode=largePopup";
+	newwin = window.open(url,"","width="+width+",height="+height+",toolbar=0,scrollbars=1,resizable=0,status=0");
 	newwin.moveTo(posx,posy);
 	newwin.focus();
 }
