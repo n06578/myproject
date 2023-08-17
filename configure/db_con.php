e06349ac30fd90b4da77436fdcc0c2c25117174b
na younah <n06578@gmail.com>
2023-08-17 15:04:39 +0900

    db connection

--- configure/db_con.php
+++ configure/db_con.php
@@ -1,8 +1,8 @@
 <?php 
 include $_SERVER['DOCUMENT_ROOT'] . "/configure/mysql.php";
-$host='localhost';												# 호스트명 또는 IP
+$host='z4.pims.co.kr';												# 호스트명 또는 IP
 $user='n06578';												# Mysql 유저이름
-$dbpass='skdbsdkWkd';												# DB 패스워드
+$dbpass='n06578';												# DB 패스워드
 $dbname='n06578';	
 										# 데이타 베이스 이름
 
